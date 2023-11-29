b87826865c2720c82fcae7498287bb1900b1ae43
ostermanj <ostermanj@gmail.com>
2023-11-29 02:40:17 +0000

    Deploying to PROD from  @ bec46ce8daa7b809632bd5d16a135ddf88bc90c8 🚀

--- es/src/index.js
+++ es/src/index.js
@@ -4,5 +4,5 @@ function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key i
 function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return typeof key === "symbol" ? key : String(key); }
 function _toPrimitive(input, hint) { if (typeof input !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (typeof res !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
 import init from './init.js';
-import state from './state.js';
+import state from './state/index.js';
 export default _objectSpread(_objectSpread({}, init), state);
