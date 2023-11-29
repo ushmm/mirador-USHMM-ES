2eea520ec020ab145b2cdbe8855e6099932cfe5d
ostermanj <ostermanj@gmail.com>
2023-11-29 02:46:15 +0000

    Deploying to PROD from  @ aeab7a2503411e913de6570154dd735dacb775d6 🚀

--- es/src/state/index.js
+++ es/src/state/index.js
@@ -1,8 +1,8 @@
-import * as actions from './actions.js';
-import * as reducers from './reducers.js';
-import * as sagas from './sagas.js';
-import * as selectors from './selectors.js';
-import createStore from './createStore.js';
+import * as actions from './actions/index.js';
+import * as reducers from './reducers/index.js';
+import * as sagas from './sagas/index.js';
+import * as selectors from './selectors/index.js';
+import createStore from './createStore/index.js';
 export default {
   actions: actions,
   createStore: createStore,
